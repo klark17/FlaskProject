fc836d7e131ec063658097c04b843668e6ebe240
Kam <kamerynlarkins@yahoo.com>
2019-08-19 22:01:21 -0400

    added flask-admin for admin side

--- lessonfinder/__init__.py
+++ lessonfinder/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 db = SQLAlchemy(app)
-
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+from lessonfinder.models import Lesson
+
+admin = Admin(app)
+admin.add_view(ModelView(Lesson, db.session))
 
 from lessonfinder import routes
 
