1ad705ecdcc5d0c5677f52079208041a29a5e474
Kam <kamerynlarkins@yahoo.com>
2020-05-14 23:25:04 -0400

    added signup user behavior in test

--- lessonfinder/locust_tests/locustfile.py
+++ lessonfinder/locust_tests/locustfile.py
@@ -1,38 +1,66 @@
-from locust import HttpLocust, TaskSet, task
-from werkzeug.test import Client
-from werkzeug.testapp import test_app
-import time
-import pdb
+from locust import HttpLocust, TaskSet, task, seq_task
 import re
+import datetime
+import random
+# import names
 # locust -f locustfile.py --no-web -c 1000 -r 100 --host=htps://127.0.0.1:5000
 # locust -f locustfile.py --host=http://127.0.0.1:5000
 
-# class WerkzeugClient(Client):
-#     def __getattr__(self, name):
-#         func = Client.__getattr__(self, name)
-#
-#         def wrapper(*args, **kwargs):
-#             start_time = time.time()
-#             try:
-#                 result = func(*args, **kwargs)
-#             except Client.Fault as e:
-#                 total_time = int((time.time() - start_time) * 1000)
-#                 events.request_failure.fire(request_type="werkzeug", name=name, response_time=total_time, exception=e)
-#             else:
-#                 total_time = int((time.time() - start_time) * 1000)
-#                 events.request_success.fire(request_type="werkzeug", name=name, response_time=total_time,
-#                                             response_length=0)
-#         return wrapper
-#
-# class WerkzeugLocust(User):
-#
-#     abstract = True
-#     def __init__(self, *args, **kwargs):
-#         super(WerkzeugLocust, self).__init__(*args, **kwargs)
-#         self.client = WerkzeugClient(test_app)
+
+class NewUser(TaskSet):
+    id = 10
+
+    @task
+    def home(self):
+        self.client.get("/about")
+
+    # @seq_task(1)
+    # def search(self):
+    #     params = {}
+    #     self.client.get("/search")
+    #     self.client.post("/search", params)
+
+    # @seq_task(2)
+    # def failed_register(self):
+    #     self.client.post("/register")
+
+    @task
+    def signup(self):
+        id = str(self.id + 1)
+        year = random.randrange(1960, 2001)
+        month = random.randrange(1, 13)
+        day = random.randrange(1, 29)
+        params = {"fName": "Test" + id,
+                  "lName": "User",
+                  "email": "test" + id + "user@mail.com",
+                  "birthday": datetime.date(year, month, day),
+                  "username":"Test" + id + "User",
+                  "password": "thisI5theTeSt" + id}
+        self.client.get("/signup")
+        self.client.post("/signup", params)
+
+    # def login(self):
+    # repeated logic that for passing a token
+        # response = self.client.get('/user/sign-in')
+        # pattern = 'csrf_token.*csrf_token.*value[=]["](.*)["]'
+        # result = re.search(pattern, response.text)
+        # csrf_token = str(result.group(1))
+        # self.client.post('/user/sign-in', {'username': 'Test1User',
+        #                                    'password': 'test',
+        #                                    'next': '/',
+        #                                    'reg_next': '/',
+        #                                    'csrf_token': csrf_token,
+        #                                    'submit': 'Sign in'})
+
+    # @seq_task(4)
+    # def profile(self):
+    #     self.client.get("/profile")
+
+    # def on_stop(self):
+    #     self.client.post("/user/sign-out", {"username":"Test1User", "password":"test"})
 
 
-class UserBehavior(TaskSet):
+class ExistingUser(TaskSet):
 
     def on_start(self):
         response = self.client.get('/user/sign-in')
@@ -54,11 +82,15 @@ class UserBehavior(TaskSet):
     def profile(self):
         self.client.request("get", "/profile", auth=("Test1User", "test"))
 
+    # seq_task: search for lesson
+    # seq_task select to register
+
     def on_stop(self):
         self.client.post("/user/sign-out", {"username":"Test1User", "password":"test"})
 
 
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+    task_set = NewUser
     min_wait = 10000
     max_wait = 30000
+
